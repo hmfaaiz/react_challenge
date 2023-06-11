991ecadebbd42f58fefa3aa889fe2ab538a1ead2
faaiz <email>
2023-06-11 11:08:11 +0500

    removed unusable files & created two components in one file.

---
+++ src/components/TwoComponent.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+export function Component1() {
+  return (
+    <div>
+      <h1> Component 1</h1>
+    </div>
+  )
+}
+export  function Component2() {
+  return (
+    <div>
+      <h1> Component 2</h1>
+    </div>
+  )
+}
+
+
