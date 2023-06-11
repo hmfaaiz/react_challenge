106ae87af02c8de057318f3dac3437b19a0e183b
faaiz <email>
2023-06-11 11:56:58 +0500

    update vakue by variable

--- src/components/clickEvent.js
+++ src/components/clickEvent.js
@@ -1,15 +1,18 @@
 import React from 'react'
 
 function ClickEvent() {
-    const click=()=>{
-        alert("Button is Clicked")
+    let course = "React Challenge"
+    const click = () => {
+        course="React"
+        alert(course)
     }
-  return (
-    <div>
-        <h1>Click Event</h1>
-      <button onClick={click}>Click Here</button>
-    </div>
-  )
+    return (
+        <div>
+            <h1>{course}: Click Event</h1>
+            <button onClick={click}>Click Here</button>
+            <button onClick={() => { alert("Hello") }}>Click Me</button>
+        </div>
+    )
 }
 
 export default ClickEvent;
