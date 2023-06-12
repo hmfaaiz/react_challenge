307e3ca22775d99c76f82dc815cc6463c71ec0e1
faaiz <email>
2023-06-12 22:02:31 +0500

    Hide and Show button is added

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,14 +1,22 @@
 
 import React, { useState } from 'react'
 function Form() {
-    const [input,setInput]=useState()
-  return (
-    <div>
-        <h1>{input}</h1>
-      <label>Name:</label>
-      <input onChange={(e)=>setInput(e.target.value)}></input>
-    </div>
-  )
+    const [input, setInput] = useState()
+    const [status, setStatus] = useState(true)
+    return (
+        <div>
+            {
+                status ?
+                    <h1> {input}</h1> : null
+            }
+
+            <label>Name:</label>
+            <input onChange={(e) => setInput(e.target.value)}></input>
+            <button onClick={() => setStatus(false)}>Hide</button>
+            <button onClick={() => setStatus(true)}>Show</button>
+
+        </div >
+    )
 }
 
 export default Form
