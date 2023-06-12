8e2b0335eac09b210d5edc18591dd333343a82dd
faaiz <email>
2023-06-12 22:05:38 +0500

    toggle is also added

--- src/components/Form.js
+++ src/components/Form.js
@@ -14,6 +14,7 @@ function Form() {
             <input onChange={(e) => setInput(e.target.value)}></input>
             <button onClick={() => setStatus(false)}>Hide</button>
             <button onClick={() => setStatus(true)}>Show</button>
+            <button onClick={() => setStatus(!status)}>Toggle</button>
 
         </div >
     )
