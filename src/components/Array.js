e4e1204b32e50a9c79ae6a7e6884c448298acc86
faaiz <email>
2023-06-13 15:35:48 +0500

    Used bootstrap in table,search by age

--- src/components/Array.js
+++ src/components/Array.js
@@ -1,6 +1,10 @@
-import React from 'react'
+import React, { useState } from 'react'
+
+import { Table, InputGroup, Form, Col, Row } from "react-bootstrap"
 
 function Array() {
+    const [filter, setFilter] = useState()
+    console.log(filter)
 
     const student = [
         { "name": "Dani", "email": "dani@gmail.com", "age": 20 },
@@ -9,38 +13,50 @@ function Array() {
         { "name": "David", "email": "david@gmail.com", "age": 15 },
         { "name": "Shayan", "email": "shay@gmail.com", "age": 30 },
     ]
-    for (let i = 0; i < student.length; i++) {
-        console.log(student[i])
 
-    }
+    // for (let i = 0; i < student.length; i++) {
+    //     console.log(student[i])
+
+    // }
     return (
         <div>
-            <h1>Data</h1>
-            <table border="2">
+
+            <Row className="m-5">
+                <Form.Group as={Col}>
+                    <h1>Student Data</h1>
+                </Form.Group>
+                <Form.Group as={Col}>
+                    <Form.Control className="mb-3" onChange={(e) => setFilter(e.target.value)} type="number" placeholder='search age' />
+                </Form.Group>
+            </Row>
+
+
+            <Table striped="columns" variant='dark'>
                 <thead>
                     <tr>
-                    <th>S no.</th>
-                    <th>Name</th>
-                    <th>Email</th>
-                    <th>Age</th>
+                        <th>S no.</th>
+                        <th>Name</th>
+                        <th>Email</th>
+                        <th>Age</th>
                     </tr>
                 </thead>
 
                 <tbody>
 
-                    {student.map((item) =>
+                    {student.map((item, index) =>
+                        item.age <= filter ?
 
-                        <tr>
-                            <td>{1}</td>
-                            <td>{item.name}</td>
-                            <td>{item.email}</td>
-                            <td>{item.age}</td>
-                        </tr>
-
+                            <tr key={index}>
+                                <td>{index + 1}</td>
+                                <td>{item.name}</td>
+                                <td>{item.email}</td>
+                                <td>{item.age}</td>
+                            </tr>
+                            : null
                     )}
 
                 </tbody>
-            </table>
+            </Table>
 
         </div>
     )
